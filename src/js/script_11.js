c41ae5e2c15ecd7555b8907bd25409a2a2571daa
Mary <mariya.sedna@gmail.com>
2025-03-08 19:35:47 -0300

    page eleven

---
+++ src/js/script_11.js
@@ -0,0 +1,53 @@
+const button1 = document.querySelector('.eleven_ans-s_1'); 
+const elementsGroup1 = document.querySelectorAll('.eleven_ans-s_1, .press, .ready'); 
+ 
+button1.addEventListener('click', () => {
+  elementsGroup1.forEach(el => {
+    el.classList.toggle('active');
+  });
+});
+
+const button2 = document.querySelector('.eleven_ans-s_2'); 
+const elementsGroup2 = document.querySelectorAll('.eleven_ans-s_2, .press, .ready'); 
+ 
+button2.addEventListener('click', () => {
+  elementsGroup2.forEach(el => {
+    el.classList.toggle('active');
+  });
+});
+
+const button3 = document.querySelector('.eleven_ans-s_3'); 
+const elementsGroup3 = document.querySelectorAll('.eleven_ans-s_3, .press, .ready'); 
+ 
+button3.addEventListener('click', () => {
+  elementsGroup3.forEach(el => {
+    el.classList.toggle('active');
+  });
+});
+
+const button4 = document.querySelector('.eleven_ans-s_4'); 
+const elementsGroup4 = document.querySelectorAll('.eleven_ans-s_4, .press, .ready'); 
+ 
+button4.addEventListener('click', () => {
+  elementsGroup4.forEach(el => {
+    el.classList.toggle('active');
+  });
+});
+
+const button5 = document.querySelector('.eleven_ans-s_5'); 
+const elementsGroup5 = document.querySelectorAll('.eleven_ans-s_5, .press, .ready'); 
+ 
+button5.addEventListener('click', () => {
+  elementsGroup5.forEach(el => {
+    el.classList.toggle('active');
+  });
+});
+
+const button6 = document.querySelector('.eleven_ans-s_6'); 
+const elementsGroup6 = document.querySelectorAll('.eleven_ans-s_6, .press, .ready'); 
+ 
+button6.addEventListener('click', () => {
+  elementsGroup6.forEach(el => {
+    el.classList.toggle('active');
+  });
+});
